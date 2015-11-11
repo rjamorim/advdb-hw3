8d02a1ca35a49cc7835edb5f1b84bbd6e458413a
pff2108 <pff2108@columbia.edu>
2015-11-11 17:56:05 -0500

    v1

--- assoc.py
+++ assoc.py
@@ -1,5 +1,20 @@
-# Association Rules Mining
+# Association Rules Mining - hw3
 # Advanced Database Systems
 # Pedro Ferro Freitas - pff2108
 # Roberto Jose de Amorim - rja2139
+
+data_file = 'INTEGRATED-DATASET.csv'
+min_sup = 0.3
+min_conf = 0.5
+
+# data_file = argv[2]
+# min_sup = float(argv[3])
+# min_conf = float(argv[4])
+
+
+print '\n==Frequent itemsets (min_sup=' + str(100 * min_sup) + '%)'
+print '[pen], 100%'
+
+print '\n==High-confidence association rules (min_conf=' + str(100 * min_conf) + '%)'
+print '[diary] => [pen] (Conf: 100.0%, Supp: 75%)'
 
